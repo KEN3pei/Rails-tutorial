d0812fd896cb69ed9f120ce45fc2010f35435b9d
KEN3pei <kensuke1202left@icloud.com>
2020-08-27 11:35:38 +0000

    update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  def full_title(set_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if set_title.empty?
+      base_title
+    else
+      set_title + " | " + base_title
+    end
+  end
 end
