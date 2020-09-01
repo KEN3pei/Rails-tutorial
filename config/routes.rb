8ddd08f5bddece257ed4a110691a0e800bc86ea9
”kensuke” <kensuke1202left@icloud.com>
2020-09-01 23:44:59 +0900

    8章 tutorial終了

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
   
   get '/signup', to:'users#new'
   post '/signup', to:'users#create'
+
+  get '/login', to:'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   
   resources :users, only: [:index, :show]
   # get '/users', to:'users#index'
